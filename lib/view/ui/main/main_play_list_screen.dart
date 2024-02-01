9502c32e89893a8b5ccd4571bbe997be644f8de7
Dalso <flskwhdhk@gmail.com>
2024-02-01 17:56:39 +0900

    feat : 나만의 플레이리스트 담는 페이지 작성

---
+++ lib/view/ui/main/main_play_list_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class MainPlayListScreen extends StatelessWidget {
+  const MainPlayListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: const Text('없다'));
+  }
+}
